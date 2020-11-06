b98fe3669dc879db4fa382731fb0462a89be74fe
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-11-06 14:00:44 +0100

    feat: social icons

---
+++ inc/config.php
@@ -0,0 +1,7 @@
+<?php
+
+class Config {
+  static $facebookUrl="https://facebook.com/Siejmy";
+  static $instagramUrl="https://instagram.com/Siejmy";
+  static $twitterUrl="https://teitter.com/Siejmy";
+}
