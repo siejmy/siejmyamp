37b12ee77a28f3360086ed14d753c35353f2c7e1
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-11-07 00:35:56 +0100

    fix: style footer

--- footer.php
+++ footer.php
@@ -13,13 +13,29 @@
 
 ?>
 
-<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
 <footer id="site-footer" role="contentinfo" class="header-footer-group">
-Footera
+	<div class="footer-content">
+		<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+	</div>
 </footer><!-- #site-footer -->
 
 <style>
+footer {
+	margin: 0 auto;
+	margin-left: -10%;
+	width: 120%;
+	padding-top: 4%;
+	padding-left: 14%;
+	padding-right: 14%;
+	background: var(--overlays-bg-dark);
+	box-shadow: inset 0px 10px 10px 8px rgba(0,0,0,0.47);
+	color: white;
+}
 
+.footer-content {
+	margin: 0 auto;
+	max-width: 1300px;
+}
 </style>
 
 <?php wp_footer(); ?>
