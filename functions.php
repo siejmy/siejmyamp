d5fd1295158f7c9c861eec12f98ca39180a6e39e
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-11-06 11:32:00 +0100

    fix: remove starter content

--- functions.php
+++ functions.php
@@ -122,16 +122,6 @@ function siejmyamp_theme_support() {
 	// Add support for responsive embeds.
 	add_theme_support( 'responsive-embeds' );
 
-	/*
-	 * Adds starter content to highlight the theme on fresh sites.
-	 * This is done conditionally to avoid loading the starter content on every
-	 * page load, as it is a one-off operation only needed once in the customizer.
-	 */
-	if ( is_customize_preview() ) {
-		require get_template_directory() . '/inc/starter-content.php';
-		add_theme_support( 'starter-content', siejmyamp_get_starter_content() );
-	}
-
 	// Add theme support for selective refresh for widgets.
 	add_theme_support( 'customize-selective-refresh-widgets' );
 
