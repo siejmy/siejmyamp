364a45e748b18663361a9bd6ac82d6b8999ce772
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-11-13 13:09:23 +0100

    fix: allow contributors to upload images

--- functions.php
+++ functions.php
@@ -381,6 +381,14 @@ function siejmyamp_get_elements_array() {
 	return apply_filters( 'siejmyamp_get_elements_array', $elements );
 }
 
+//Let Contributor Role to Upload Media
+if ( current_user_can('contributor') && !current_user_can('upload_files') )
+    add_action('admin_init', 'allow_contributor_uploads');
+function allow_contributor_uploads() {
+    $contributor = get_role('contributor');
+    $contributor->add_cap('upload_files');
+}
+
 
 /******************************************/
 /* UTILITIES                              */
