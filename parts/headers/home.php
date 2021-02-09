955a453068248923c817cad5a43da02ba37434aa
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2021-02-10 00:29:01 +0100

    fix: align the who we are tooltip

--- parts/headers/home.php
+++ parts/headers/home.php
@@ -9,20 +9,13 @@
   .header-subline {
     width: 100%;
     height: 6rem;
-    z-index: 5;
     position: relative;
-    background: rgba(255, 255, 255, 0.7);
-    box-shadow: 0 2px 5px rgba(0,0,0,0.15);
+    padding-left: 10%;
   }
 
   .header-subline .megamenu-section {
     float: right;
     padding-top: 5px;
     padding-right: 5px;
-  }
-
-  .socico-horizontal {
-    padding-top: 5px;
-    padding-left: 5px;
   }
 </style>
