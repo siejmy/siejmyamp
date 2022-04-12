26cbfe94bbc840b28793c89956895a74bf0d729c
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-04-12 15:34:03 +0200

    feat: pagination

--- template-parts/pagination.php
+++ template-parts/pagination.php
@@ -9,20 +9,14 @@
  * @since SiejmyAMP 1.0
  */
 
-/**
- * Translators:
- * This text contains HTML to allow the text to be shorter on small screens.
- * The text inside the span with the class nav-short will be hidden on small screens.
- */
-
 $prev_text = sprintf(
 	'%s <span class="nav-prev-text">%s</span>',
 	'<span aria-hidden="true">&larr;</span>',
-	__( 'Newer <span class="nav-short">Posts</span>', 'siejmyamp' )
+	'Poprzednie',
 );
 $next_text = sprintf(
 	'<span class="nav-next-text">%s</span> %s',
-	__( 'Older <span class="nav-short">Posts</span>', 'siejmyamp' ),
+	'Kolejne',
 	'<span aria-hidden="true">&rarr;</span>'
 );
 
@@ -34,25 +28,19 @@ $posts_pagination = get_the_posts_pagination(
 	)
 );
 
-// If we're not outputting the previous page link, prepend a placeholder with `visibility: hidden` to take its place.
 if ( strpos( $posts_pagination, 'prev page-numbers' ) === false ) {
 	$posts_pagination = str_replace( '<div class="nav-links">', '<div class="nav-links"><span class="prev page-numbers placeholder" aria-hidden="true">' . $prev_text . '</span>', $posts_pagination );
 }
 
-// If we're not outputting the next page link, append a placeholder with `visibility: hidden` to take its place.
 if ( strpos( $posts_pagination, 'next page-numbers' ) === false ) {
 	$posts_pagination = str_replace( '</div>', '<span class="next page-numbers placeholder" aria-hidden="true">' . $next_text . '</span></div>', $posts_pagination );
 }
 
 if ( $posts_pagination ) { ?>
 
-	<div class="pagination-wrapper section-inner">
-
-		<hr class="styled-separator pagination-separator is-style-wide" aria-hidden="true" />
-
-		<?php echo $posts_pagination; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- already escaped during generation. ?>
-
-	</div><!-- .pagination-wrapper -->
+	<nav class="pagination" role="pagination">
+		<?php echo $posts_pagination; ?>
+	</nav>
 
 	<?php
 }
