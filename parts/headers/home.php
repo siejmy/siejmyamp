9b8fa5686188b66f2f07f8c7738df4e589831ca1
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-05-05 19:36:55 +0200

    refactor: move siejmy.pl specific components to siejmyplamp child theme

--- parts/headers/home.php
+++
@@ -1,2 +0,0 @@
-<?php get_template_part('parts/headers/base') ?>
-<?php get_template_part('parts/megamenu/menu') ?>
--- parts/who-we-are-tooltip.php
+++
@@ -1,7 +0,0 @@
-<?php require_once(dirname(__FILE__) . '/../config.php') ?>
-
-<span class="who-we-are-tooltip">
-  <span>
-    Sprawdź, <a href="<?php echo(Config::$whoWeAreUrl) ?>">kim jesteśmy</a>!
-  </span>
-</span>
