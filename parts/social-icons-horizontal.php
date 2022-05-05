bebb18c8aea54826f3f94d2b68bf91cd1741377c
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-05-05 18:23:52 +0200

    feat: white social icons

--- parts/social-icons-horizontal.php
+++ parts/social-icons-horizontal.php
@@ -1,9 +1,14 @@
 <?php require_once(dirname(__FILE__) . '/../config.php') ?>
-
+<?php
+$iconSuffix = '';
+if (isset($args['icons_type'])) {
+  $iconSuffix = '-' . $args['icons_type'];
+}
+?>
 <span class="socico-horizontal">
   <a href="<?php echo(Config::$facebookUrl) ?>">
     <amp-img
-      src="<?php bloginfo('template_url'); ?>/assets/icons/facebook-square.svg"
+      src="<?php bloginfo('template_url'); ?>/assets/icons/facebook-square<?php echo $iconSuffix ?>.svg"
       width="26"
       height="26"
       alt="Facebook siejmy"
@@ -13,7 +18,7 @@
   </a>
   <a href="<?php echo(Config::$instagramUrl) ?>">
     <amp-img
-      src="<?php bloginfo('template_url'); ?>/assets/icons/instagram.svg"
+      src="<?php bloginfo('template_url'); ?>/assets/icons/instagram<?php echo $iconSuffix ?>.svg"
       width="26"
       height="26"
       alt="Instagram siejmy"
@@ -23,7 +28,7 @@
   </a>
   <a href="<?php echo(Config::$twitterUrl) ?>">
     <amp-img
-      src="<?php bloginfo('template_url'); ?>/assets/icons/twitter.svg"
+      src="<?php bloginfo('template_url'); ?>/assets/icons/twitter<?php echo $iconSuffix ?>.svg"
       width="26"
       height="26"
       alt="Twitter siejmy"
