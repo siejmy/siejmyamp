f51517272bdad79274c926fb306bc2d7133761c3
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-05-06 16:57:17 +0200

    perf: avatar as plain img

--- parts/content/author.php
+++ parts/content/author.php
@@ -2,27 +2,18 @@
   <?php
   require_once( ABSPATH . 'wp-content/plugins/siejmycommon-plugin/classes/ImageRenderer.php');
   $imgRenderer = new ImageRenderer();
+  $avatarSize = 256;
 
-  if(function_exists('get_avatar_data')) {
-    $avatarData = get_avatar_data($post->post_author);
-    echo('<!-- avatar_data');
-    print_r($avatarData);
-    echo('-->');
-    $url = $avatarData['url'];
-    $id = empty($url) ? '' : attachment_url_to_postid($url);
-    $alt = '';
-    if($id) {
-      echo $imgRenderer->renderImgByAttachmentId($id, $alt, array(
-        'showSrcset' => false,
-        'default_size' => 'siejmy_100',
-      ));
-    }
-  } else {
-    echo('<!-- missing fn get_avatar_data -->');
-  }
+  $authorName = get_the_author_meta('display_name', $post->post_author);
+  $avatarData = get_avatar_data($post->post_author, array('size' => $avatarSize));
+  echo('<!-- avatar_data');
+  print_r($avatarData);
+  echo('-->');
+  $url = $avatarData['url'];
+  echo '<img src="' . $url . '" alt="Avatar ' . $authorName . '" width="' . $avatarSize . '" height="' . $avatarSize . '" />'
   ?>
   <a class="author-name" rel="author" href="<?php echo esc_url( get_author_posts_url( $post->post_author ) ); ?>">
-    <?php echo get_the_author_meta('display_name', $post->post_author) ?>
+    <?php echo $authorName ?>
   </a>
 
     <?php echo wp_kses_post( wpautop( get_the_author_meta( 'description', $post->post_author ) ) ); ?>
