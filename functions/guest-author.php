7d9b359e2b34e527ce52c531508ca2e606d9c1a8
Jędrzej B. Lewandowski, MD <jedrzejblew@gmail.com>
2023-02-27 23:00:00 +0100

    fix: override structured author display name

--- functions/guest-author.php
+++ functions/guest-author.php
@@ -8,9 +8,17 @@ function siejmyamp_guest_author_name($author_name) {
     return $author_name;
 }
 add_filter('the_author', 'siejmyamp_guest_author_name');
-add_filter('get_the_author', 'siejmyamp_guest_author_name');
 add_filter('get_the_author_display_name', 'siejmyamp_guest_author_name');
 add_filter('get_the_author_last_name', 'siejmyamp_guest_author_name');
+
+function siejmyamp_guest_author_override_object($author) {
+	$guest_author = get_post_meta(get_the_ID(), 'guest_author', true);
+	if($guest_author) {
+		$author->display_name = $guest_author;
+	}
+	return $author;
+}
+add_filter('get_the_author', 'siejmyamp_guest_author_override_object', 10, 1);
 
 function siejmyamp_guest_author_empty_value($prev_value) {
 	$guest_author = get_post_meta(get_the_ID(), 'guest_author', true);
